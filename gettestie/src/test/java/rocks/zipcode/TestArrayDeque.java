382b62edf0fda7f71af969bb160214f9e79ed660
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-13 09:46:00 -0500

    Done with ArrayDeque

--- gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
@@ -1,152 +1,133 @@
 package rocks.zipcode;
 import org.junit.Test;
 import gettestie.src.main.java.rocks.zipcode.Person;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 
 import static org.junit.Assert.*;
-
-
-public class TestLinkedList {
+public class TestArrayDeque {
 
     @Test
-    public void testLinkedListAdd(){
+    public void testArrayDequeAdd(){
         //Person person=new Person;
         Person me = new Person("Gunjan", 1988);
         Person santosh = new Person("Santosh", 1975);
         Person tom = new Person("Tom", 1955);
         me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
+        ArrayDeque<Person> expected = new ArrayDeque<>();
         expected.add(me);
         expected.add(santosh);
         assertTrue(expected.add(tom));;
     }
     @Test
-    public void testLinkedListRemove(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-
-        assertEquals(me, expected.remove());;
-    }
-
-    @Test
-    public void testLinkedListRemoveFirst(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-
-        assertEquals(me, expected.removeFirst());;
-    }
-    @Test
-    public void testLinkedListRemoveLast(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-
-        assertEquals(tom, expected.removeLast());;
-    }
-
-    @Test
-    public void testLinkedListClearAndEmpty(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-        expected.clear();
-
-        assertTrue(expected.isEmpty());;
-    }
-
-    @Test
-    public void testLinkedListOfferFirst(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        Person setP=new Person("To Set" ,1981);
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-        Boolean output = expected.offerFirst(setP);
-       // assertEquals(true,expected.offerFirst(tom));;
-        assertEquals(setP,expected.peekFirst());
-        assertEquals(4,expected.size());
-    }
-    @Test
-    public void testLinkedListPeek(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-       assertEquals(me, expected.peek());;
-    }
-
-    @Test
-    public void testLinkedListSet(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        Person setP=new Person("To Set" ,1981);
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-        assertEquals(santosh, expected.set(1,setP));;
-    }
-
-    @Test
-    public void testLinkedListSize(){
-        Person me = new Person("Gunjan", 1988);
-        Person santosh = new Person("Santosh", 1975);
-        Person tom = new Person("Tom", 1955);
-        me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.add(me);
-        expected.add(santosh);
-        expected.add(tom);
-        assertEquals(3, expected.size());;
-    }
-    @Test
-    public void testLinkedListPush(){
+    public void testArrayDequeClearAndIsEmpty(){
         //Person person=new Person;
         Person me = new Person("Gunjan", 1988);
         Person santosh = new Person("Santosh", 1975);
         Person tom = new Person("Tom", 1955);
         me.setAddress("Street 1","Town 1" ,"Postal Code");
-        LinkedList<Person> expected = new LinkedList<Person>();
-        expected.push(me);
-        expected.push(santosh);
-        expected.push(tom);
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.add(santosh);
+        expected.clear();
+        assertTrue(expected.isEmpty());;
+    }
+    @Test
+    public void testArrayDequeContains(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.add(santosh);
 
-        assertEquals(tom,expected.pop());;  //LIFO concept of Stack
+        assertTrue(expected.contains(me));;
+    }
+    @Test
+    public void testArrayDequePeek(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.add(santosh);
+
+        assertEquals(me,expected.peek());;
+    }
+    @Test
+    public void testArrayDequeOffer(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.offerFirst(santosh);
+
+        assertEquals(santosh,expected.peek());;
     }
 
-}
+    @Test
+    public void testArrayDequePollOnEmpty(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.offerFirst(santosh);
+        expected.clear();
+        assertEquals(null,expected.poll());;
+    }
 
+
+    @Test
+    public void testArrayDequeRemove(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.add(me);
+        expected.offerFirst(santosh);
+
+        assertEquals(true,expected.remove(santosh));;
+    }
+    @Test
+    public void testArrayDequePushAndPop(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.push(me);
+        expected.push(tom);
+        assertEquals(tom,expected.pop());;
+    }
+    @Test
+    public void testArrayDequeSize(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        ArrayDeque<Person> expected = new ArrayDeque<>();
+        expected.push(me);
+        expected.push(tom);
+        assertEquals(2,expected.size());;
+    }
+
+
+
+}
