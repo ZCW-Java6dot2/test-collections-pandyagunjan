2b5996a05eb1a72ed8d5398c3491e02e4616f8a4
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-13 07:17:58 -0500

    Done with LinkedList

--- gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
+++ gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
@@ -86,13 +86,69 @@ public class TestLinkedList {
         Person santosh = new Person("Santosh", 1975);
         Person tom = new Person("Tom", 1955);
         me.setAddress("Street 1","Town 1" ,"Postal Code");
+        Person setP=new Person("To Set" ,1981);
         LinkedList<Person> expected = new LinkedList<Person>();
         expected.add(me);
         expected.add(santosh);
         expected.add(tom);
-        expected.clear();
-
-        assertEquals(me,expected.offerFirst(me));;
+        Boolean output = expected.offerFirst(setP);
+       // assertEquals(true,expected.offerFirst(tom));;
+        assertEquals(setP,expected.peekFirst());
+        assertEquals(4,expected.size());
     }
+    @Test
+    public void testLinkedListPeek(){
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        LinkedList<Person> expected = new LinkedList<Person>();
+        expected.add(me);
+        expected.add(santosh);
+        expected.add(tom);
+       assertEquals(me, expected.peek());;
+    }
+
+    @Test
+    public void testLinkedListSet(){
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        Person setP=new Person("To Set" ,1981);
+        LinkedList<Person> expected = new LinkedList<Person>();
+        expected.add(me);
+        expected.add(santosh);
+        expected.add(tom);
+        assertEquals(santosh, expected.set(1,setP));;
+    }
+
+    @Test
+    public void testLinkedListSize(){
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        LinkedList<Person> expected = new LinkedList<Person>();
+        expected.add(me);
+        expected.add(santosh);
+        expected.add(tom);
+        assertEquals(3, expected.size());;
+    }
+    @Test
+    public void testLinkedListPush(){
+        //Person person=new Person;
+        Person me = new Person("Gunjan", 1988);
+        Person santosh = new Person("Santosh", 1975);
+        Person tom = new Person("Tom", 1955);
+        me.setAddress("Street 1","Town 1" ,"Postal Code");
+        LinkedList<Person> expected = new LinkedList<Person>();
+        expected.push(me);
+        expected.push(santosh);
+        expected.push(tom);
+
+        assertEquals(tom,expected.pop());;  //LIFO concept of Stack
+    }
+
 }
 
